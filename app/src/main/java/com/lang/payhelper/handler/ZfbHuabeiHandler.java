60b2f3470e36f8a1bd6efd06a7b2da534048a6d6
袁浪 <yuanlang@bigsfintech.com>
2020-04-13 19:30:05 +0800

    新加花呗功能

---
+++ app/src/main/java/com/lang/payhelper/handler/ZfbHuabeiHandler.java
@@ -0,0 +1,12 @@
+package com.lang.payhelper.handler;
+
+import com.lang.sekiro.api.SekiroRequest;
+import com.lang.sekiro.api.SekiroRequestHandler;
+import com.lang.sekiro.api.SekiroResponse;
+
+public class ZfbHuabeiHandler implements SekiroRequestHandler {
+    @Override
+    public void handleRequest(SekiroRequest sekiroRequest, SekiroResponse sekiroResponse) {
+
+    }
+}
